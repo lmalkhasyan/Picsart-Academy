f8c470e0d2549e0b2528a4747021738b08af2cf3
Levon <84186090+lmalkhasyan@users.noreply.github.com>
2022-09-08 21:43:31 +0400

    Update 5.cpp
    Bug Fix! :+1:

--- 3. 06.09/5.cpp
+++ 3. 06.09/5.cpp
@@ -18,6 +18,11 @@ int my_mul(int num1, int num2)
 
 int my_div(int num1, int num2)
 {
+	if(num2 == 0)
+	{
+		std::cout << "Division by Zero!" << std::endl;
+		return (-1);
+	}
 	return num1 / num2;
 }
 
